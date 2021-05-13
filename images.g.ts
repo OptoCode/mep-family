9293a1c4256988a127ad5eceb648976075dd4947
OptoCode <67488528+OptoCode@users.noreply.github.com>
2021-05-13 08:37:35 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "n2S2ND35KHSv8eGorwx8":
+            case "corn1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+. . . . . . . 7 . 5 5 . . . . . 
+. . . . . . . 7 5 5 7 . . . . . 
+. . . . . . . 7 5 7 . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+. . . . 5 5 . 7 . . . . . . . . 
+. . . . 7 5 5 7 . . . . . . . . 
+. . . . . 7 5 7 . . . . . . . . 
+. . . . . . 7 7 . . . . . . . . 
+. . . . . . . 7 5 5 . . . . . . 
+. . . . . . . 7 5 7 . . . . . . 
+. . . . . . . 7 7 . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ tilemap.g.ts
@@ -0,0 +1,83 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`100010000b0b0c0c0c0a0c0c0c0c0c0c0c0c0c0a0b01030303030303030303030303050b0c0204041312140404040404040d060c0c02040416171504040404040404060c0c020404100f1104040404040404060a0c0204040d040404040404040404060c0a02040404040404131214040404060b0b02040404040404100f11040404060c0b02040404040404040404040404060c0b02040404040404040404040404060b0a02041312140404040404040404060c0b0204100f11040404040404040e060c0b02040404040404131214040404060a0b02040d04040404100f11040404060c0b07080808080808080808080808090c0b0a0c0c0b0c0c0b0c0c0a0c0c0b0c0c`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . 2 . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . 2 . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . 2 . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . 2 . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.castle.tilePath1,sprites.castle.tilePath4,sprites.castle.tilePath2,sprites.castle.tilePath5,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath7,sprites.castle.tilePath8,sprites.castle.tilePath9,sprites.castle.tileGrass2,sprites.castle.tileGrass3,sprites.castle.tileGrass1,myTiles.tile1,myTiles.tile2,myTiles.tile4,myTiles.tile3,myTiles.tile5,myTiles.tile7,myTiles.tile6,myTiles.tile8,myTiles.tile10,myTiles.tile9,myTiles.tile11], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "dirtTopLeft":
+            case "tile6":return tile6;
+            case "dirtTop":
+            case "tile7":return tile7;
+            case "dirtTopRight":
+            case "tile8":return tile8;
+            case "dirtRightMiddle":
+            case "tile10":return tile10;
+            case "dirtLeftMiddle":
+            case "tile9":return tile9;
+            case "dirtCenter":
+            case "tile11":return tile11;
+            case "dirtBottomRight":
+            case "tile5":return tile5;
+            case "dirtLeftBottom":
+            case "tile3":return tile3;
+            case "dirtBottom":
+            case "tile4":return tile4;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
