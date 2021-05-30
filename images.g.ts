3499fa813ca6e0e6a5fc59dc761b3242b619388c
OptoCode <67488528+OptoCode@users.noreply.github.com>
2021-05-30 13:01:20 -0500

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -22,6 +22,25 @@ namespace myImages {
 . . . . . . . 7 . . . . . . . . 
 . . . . . . . 7 . . . . . . . . 
 `;
+            case "k!,o6xswrZt":
+            case "farmWorker":return img`
+. . . f f f f f f . . . 
+. . f 2 2 2 2 2 2 f . . 
+. f 2 2 2 2 2 2 2 2 f . 
+f f 2 2 f f f f 2 2 f f 
+f f 2 f f f f f f 2 f f 
+f f f f 4 4 4 4 f f f f 
+f 4 e 4 4 4 4 4 4 e 4 f 
+f 4 4 f f 4 4 f f 4 4 f 
+f e 4 d d d d d d 4 e f 
+. f e d d b b d d e f . 
+. f f e 4 4 4 4 e f f . 
+e 4 f b 1 1 1 1 b f 4 e 
+4 d f 1 1 1 1 1 1 f d 4 
+4 4 f 6 6 6 6 6 6 f 4 4 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`;
         }
         return null;
     })
