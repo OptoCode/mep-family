c15518af3ecd4fe678893cd307518f833a7fc269
OptoCode <67488528+OptoCode@users.noreply.github.com>
2021-05-30 13:47:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -181,6 +181,86 @@ game.showLongText("Every year families decide due to crop seasons to move to fin
 game.showLongText("Collect crops before the crop season is over.", DialogLayout.Bottom)
 pause(1000)
 info.startCountdown(30)
+game.onUpdate(function () {
+    // set hero facing directions
+    if (farmworker.vx > 0) {
+        farmworker.setImage(img`
+            . . . . f f f f f . . . 
+            . . f f e e e e e f . . 
+            . f f e e e e e e e f . 
+            f f f f e e e e e e e f 
+            f f f f f e e e 4 e e f 
+            f f f f e e e 4 4 e e f 
+            f f f f 4 4 4 4 4 e f f 
+            f f 4 e 4 f f 4 4 e f f 
+            . f 4 d 4 d d d d f f . 
+            . f f f 4 d d b b f . . 
+            . . f e e 4 4 4 e f . . 
+            . . 4 d d e 1 1 1 f . . 
+            . . e d d e 1 1 1 f . . 
+            . . f e e f 6 6 6 f . . 
+            . . . f f f f f f . . . 
+            . . . . f f f . . . . . 
+            `)
+    } else if (farmworker.vx < 0) {
+        farmworker.setImage(img`
+            . . . f f f f f . . . . 
+            . . f e e e e e f f . . 
+            . f e e e e e e e f f . 
+            f e e e e e e e f f f f 
+            f e e 4 e e e f f f f f 
+            f e e 4 4 e e e f f f f 
+            f f e 4 4 4 4 4 f f f f 
+            f f e 4 4 f f 4 e 4 f f 
+            . f f d d d d 4 d 4 f . 
+            . . f b b d d 4 f f f . 
+            . . f e 4 4 4 e e f . . 
+            . . f 1 1 1 e d d 4 . . 
+            . . f 1 1 1 e d d e . . 
+            . . f 6 6 6 f e e f . . 
+            . . . f f f f f f . . . 
+            . . . . . f f f . . . . 
+            `)
+    } else if (farmworker.vy < 0) {
+        farmworker.setImage(img`
+            . . . . f f f f . . . . 
+            . . f f e e e e f f . . 
+            . f e e e e e e e f f . 
+            f f e f e e e e e e f f 
+            f f f e e e e e e e e f 
+            f f f e e e e e e f e f 
+            f f f f e e e e f f f f 
+            f f f f f f f f f f f f 
+            f f f f f f f f f f f f 
+            . f f f f f f f f f f . 
+            . e f f f f f f f f e . 
+            e 4 f b b b b b b f 4 e 
+            4 d f d d d d d d c d 4 
+            4 4 f 6 6 6 6 6 6 f 4 4 
+            . . . f f f f f f . . . 
+            . . . f f . . f f . . . 
+            `)
+    } else if (farmworker.vy > 0) {
+        farmworker.setImage(img`
+            . . . . f f f f . . . . 
+            . . f f e e e e f f . . 
+            . f f e e e e e e f f . 
+            f f f f 4 e e e f f f f 
+            f f f 4 4 4 e e f f f f 
+            f f f 4 4 4 4 e e f f f 
+            f 4 e 4 4 4 4 4 4 e 4 f 
+            f 4 4 f f 4 4 f f 4 4 f 
+            f e 4 d d d d d d 4 e f 
+            . f e d d b b d d e f . 
+            . f f e 4 4 4 4 e f f . 
+            e 4 f b 1 1 1 1 b f 4 e 
+            4 d f 1 1 1 1 1 1 f d 4 
+            4 4 f 6 6 6 6 6 6 f 4 4 
+            . . . f f f f f f . . . 
+            . . . f f . . f f . . . 
+            `)
+    }
+})
 forever(function () {
     if (game.runtime() < 20000) {
         music.setVolume(20)
