f6e572d2bc3bbb4ef826e4787eaa7c04d7e1eb41
OptoCode <67488528+OptoCode@users.noreply.github.com>
2021-07-30 08:42:09 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,6 +3,25 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "k!,o6xswrZt":
+            case "farmWorker":return img`
+. . . . f f f f . . . . 
+. . f f e e e e f f . . 
+. f f e e e e e e f f . 
+f f f f 4 e e e f f f f 
+f f f 4 4 4 e e f f f f 
+f f f 4 4 4 4 e e f f f 
+f 4 e 4 4 4 4 4 4 e 4 f 
+f 4 4 f f 4 4 f f 4 4 f 
+f e 4 d d d d d d 4 e f 
+. f e d d b b d d e f . 
+. f f e 4 4 4 4 e f f . 
+e 4 f b 1 1 1 1 b f 4 e 
+4 d f 1 1 1 1 1 1 f d 4 
+4 4 f 6 6 6 6 6 6 f 4 4 
+. . . f f f f f f . . . 
+. . . f f . . f f . . . 
+`;
             case "n2S2ND35KHSv8eGorwx8":
             case "corn1":return img`
 . . . . . . . . . . . . . . . . 
@@ -21,25 +40,6 @@ namespace myImages {
 . . . . . . . 7 . . . . . . . . 
 . . . . . . . 7 . . . . . . . . 
 . . . . . . . 7 . . . . . . . . 
-`;
-            case "k!,o6xswrZt":
-            case "farmWorker":return img`
-. . . f f f f f f . . . 
-. . f 2 2 2 2 2 2 f . . 
-. f 2 2 2 2 2 2 2 2 f . 
-f f 2 2 f f f f 2 2 f f 
-f f 2 f f f f f f 2 f f 
-f f f f 4 4 4 4 f f f f 
-f 4 e 4 4 4 4 4 4 e 4 f 
-f 4 4 f f 4 4 f f 4 4 f 
-f e 4 d d d d d d 4 e f 
-. f e d d b b d d e f . 
-. f f e 4 4 4 4 e f f . 
-e 4 f b 1 1 1 1 b f 4 e 
-4 d f 1 1 1 1 1 1 f d 4 
-4 4 f 6 6 6 6 6 6 f 4 4 
-. . . f f f f f f . . . 
-. . . f f . . f f . . . 
 `;
         }
         return null;
